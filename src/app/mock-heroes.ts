75c1cd0fd92f45fe3f728cb0ca937eab6e0e2f3e
O S <vystoya@gmail.com>
2018-09-13 16:26:04 +0300

    displaying a list + css

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Oyaro' },
+  { id: 12, name: 'Bryan' },
+  { id: 13, name: 'Sheila' },
+  { id: 14, name: 'Eva' },
+  { id: 15, name: 'Eric' },
+  { id: 16, name: 'Isaac' },
+  { id: 17, name: 'Irene' },
+  { id: 18, name: 'Dr Antoh' },
+  { id: 19, name: 'Jael' },
+  { id: 20, name: 'Susan' }
+];
